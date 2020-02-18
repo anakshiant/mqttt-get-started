8ee173901168e830e236b2eab1d921baf7cd854f
aaku14n <akarshgdps@gmail.com>
2020-02-18 11:15:22 +0530

    ADding

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("*", (req, res, next) => {
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/login.html"));
 });
 // create a device which will save client indexed by it's mac address
 // req {macAddress:"xxxx"}
